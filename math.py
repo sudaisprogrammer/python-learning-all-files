7c2c381b87be7e8278661fb2367c1edbe70a026e
sudaisprogrammer <sudais.programmer@gmail.com>
2026-07-25 11:23:06 -0700

    math in python

--- math.py
+++ math.py
@@ -1,7 +1,12 @@
 x = 12
-y  = 20
+y  = 2
 print(x+y)
-
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**y) # a raise to power b like x^y e.g 2^3 = 8
 x1 = 12.0
-y1 = 1.5
+y1 = 1.59
 print(x1/y1)
+print(x1//y1) # return only the integer part e.g if 3.4 then it will return 3
+
